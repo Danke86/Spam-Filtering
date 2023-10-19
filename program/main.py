80436d083c768e7517f63f3aeaf33ea3f3d5ae54
EarlySummerRain <46488780+EarlySummerRain@users.noreply.github.com>
2023-10-20 07:45:51 +0800

    Added Classifier

--- program/main.py
+++ program/main.py
@@ -3,11 +3,20 @@ from tkinter import ttk
 from tkinter import filedialog
 import os
 import decimal
+from decimal import Decimal
+import math
+
+class FileClassification:
+    def __init__(self, filename, classification, probabilitySpam):
+        self.filename = filename
+        self.classification = classification
+        self.probabilitySpam = probabilitySpam
 
 #program global variables
 bgcolor1 = "#C9E4DE"
 
 k = 0
+threshold = Decimal(0.50)
 
 spamfolderpath = ""
 hamfolderpath = ""
@@ -16,10 +25,16 @@ classifyfolderpath = ""
 spamDict = {}
 spamSize = 0
 spamNumWords = 0
+spamCount = 0
 
 hamDict = {}
 hamSize = 0
 hamNumWords = 0
+hamCount = 0
+
+mergedCount = 0
+
+classificationArray = []
 
 #functions
 def disableResize(event):
@@ -27,13 +42,15 @@ def disableResize(event):
         return "break"
 
 def openSpamFolder():
-    global spamSize, spamfolderpath, spamDict, spamNumWords
+    global spamSize, spamfolderpath, spamDict, spamNumWords, spamCount, mergedCount
+    spamCount = 0
     bag = {}
     words = []
     spamfolderpath = filedialog.askdirectory()
     if spamfolderpath:
         for filename in os.listdir(spamfolderpath):
             if filename:
+                spamCount += 1
                 file_path = os.path.join(spamfolderpath, filename)
                 f = open(file_path, 'r', encoding='latin-1')
                 lines = f.read()
@@ -64,24 +81,29 @@ def openSpamFolder():
 
                 #sort dictionary
                 spamDict = dict(sorted(bag.items()))
+
+                merged_dict = {**spamDict, **hamDict}
+                mergedCount = len(merged_dict)
         #update UI
         spamTable.delete(*spamTable.get_children())
         for word, freq in spamDict.items():
             spamTable.insert("", "end", values=(word, freq))
         spamTableLabel.config(text="Total words in Spam: {}".format(spamNumWords))
-        dictionarySizeLabel.config(text="Dictionary Size: {}".format(spamSize+hamSize))
+        dictionarySizeLabel.config(text="Dictionary Size: {}".format(mergedCount))
         totalWordsLabel.config(text="Total words: {}".format(spamNumWords+hamNumWords))
         
         printValues()
                                 
 def openHamFolder():
-    global hamSize, hamfolderpath, hamDict, hamNumWords
+    global hamSize, hamfolderpath, hamDict, hamNumWords, hamCount, mergedCount
+    hamCount = 0
     bag = {}
     words = []
     hamfolderpath = filedialog.askdirectory()
     if hamfolderpath:
         for filename in os.listdir(hamfolderpath):
             if filename:
+                hamCount += 1
                 file_path = os.path.join(hamfolderpath, filename)
                 f = open(file_path, 'r', encoding='latin-1')
                 lines = f.read()
@@ -112,26 +134,147 @@ def openHamFolder():
 
                 #sort dictionary
                 hamDict = dict(sorted(bag.items()))
+
+                merged_dict = {**spamDict, **hamDict}
+                mergedCount = len(merged_dict)
         #update UI
         hamTable.delete(*hamTable.get_children())
         for word, freq in hamDict.items():
             hamTable.insert("", "end", values=(word, freq))
         hamTableLabel.config(text="Total words in Ham: {}".format(hamNumWords))
-        dictionarySizeLabel.config(text="Dictionary Size: {}".format(spamSize+hamSize))
+        dictionarySizeLabel.config(text="Dictionary Size: {}".format(mergedCount))
         totalWordsLabel.config(text="Total words: {}".format(spamNumWords+hamNumWords))
         
         printValues()
 
 def openClassifyFolder():
-    pass
+    global classifyfolderpath
+    classifyfolderpath = filedialog.askdirectory()
+    if classifyfolderpath:
+        openedClassifyFolderLabel.config(text="Folder to Classify: {}".format(classifyfolderpath))
 
 def filterbuttonClick():
-    k = kEntry.get()
-    
+    global k, spamfolderpath, hamfolderpath, classifyfolderpath, classificationArray
+    tempk = kEntry.get()
+    #validate
+    if not is_integer(tempk):
+        popupDialog("Error", "Not an Integer!")
+        return -1
+    if int(tempk) < 0:
+        popupDialog("Error", "k cannot be a negative number")
+        return -1
+    if not spamfolderpath:
+        popupDialog("Error", "No choosen Spam Folder")
+        return -1
+    if not hamfolderpath:
+        popupDialog("Error", "No choosen Ham Folder")
+        return -1
+    if not classifyfolderpath:
+        popupDialog("Error", "No choosen Classify Folder")
+        return -1
+
+    k = int(tempk)
+
+    #Laplace Smoothing
+    #Step 1
+    p_spam = Decimal((spamCount + k) / ((spamCount+hamCount) + (2*k)))  #P(Spam)
+    p_ham = Decimal(1 - p_spam)                                        #P(Ham)
+    print("{} {}".format(p_spam,p_ham)) 
+
+    #per message
+    for filename in os.listdir(classifyfolderpath):
+        if filename:
+            file_path = os.path.join(classifyfolderpath, filename)
+            f = open(file_path, 'r', encoding='latin-1')
+            lines = f.read()
+            #tokenize
+            words = lines.replace("\n", " ").split(" ")
+
+            message = []
+            newwords = []
+
+            for word in words:
+                #clean
+                cleanword = "".join(c.lower() for c in word if c.isalpha() or c.isnumeric())
+                if cleanword != "":
+                    message.append(cleanword)
+                    if spamDict.get(word) == None and cleanword not in newwords:
+                        newwords.append(cleanword)
+            
+            
+            #Step 2
+            p_message_spam = Decimal(1.0)
+            for word in message:
+                p_message_spam *= Decimal((spamDict.get(word,0) + k) / (spamNumWords + k * (len(newwords) + mergedCount)))
+                # p_message_spam = p_message_spam * Decimal((spamDict.get(word,0) + k) / (spamNumWords + (k*((spamSize+hamSize)+len(newwords)))))
+                # p_message_spam += math.log(Decimal((spamDict.get(word, 0) + k) / (spamNumWords + k * (len(newwords) + spamSize + hamSize))))
+            print(p_message_spam)
+
+            p_message_ham = Decimal(1.0)
+            for word in message:
+                p_message_ham *= Decimal((hamDict.get(word,0) + k) / (hamNumWords + k * (len(newwords) + mergedCount)))
+                # p_message_ham = p_message_ham * Decimal((spamDict.get(word,0) + k) / (hamNumWords + (k*((spamSize+hamSize)+len(newwords)))))
+                # p_message_ham += math.log(Decimal((hamDict.get(word, 0) + k) / (hamNumWords + k * (len(newwords) + spamSize + hamSize))))
+            print(p_message_ham)
+
+            #Step 3
+            p_message = Decimal((p_message_spam*p_spam) + (p_message_ham*p_ham))
+            # print(Decimal((math.exp(p_message_spam)*p_spam) + (math.exp(p_message_ham)*p_ham)))
+            # p_message = math.log1p(Decimal((math.exp(p_message_spam)*p_spam) + (math.exp(p_message_ham)*p_ham)))
+
+            print(p_message)
+
+            p_spam_message = Decimal((p_message_spam*p_spam)/(p_message))
+            # p_spam_message = math.exp(p_message_spam + p_spam - math.log(p_message))
+            # p_spam_message = (math.exp(p_message_spam)*p_spam) / math.expm1(p_message)
+
+            print(p_spam_message)
+
+            #classify
+            if p_spam_message > threshold: #SPAM
+                newFile = FileClassification(filename, "SPAM", p_spam_message)
+                classificationArray.append(newFile)
+            else: #HAM
+                newFile = FileClassification(filename, "HAM", p_spam_message)
+                classificationArray.append(newFile)
+
+    #classify.out
+    outputFile = open("classify.out", "w", encoding="latin-1")
+    for x in classificationArray:
+        outputFile.write("{} {} {} \n".format(x.filename, x.classification, x.probabilitySpam))
+        #update UI
+        outputTable.insert("", "end", values=(x.filename, x.classification, x.probabilitySpam))
+
+    outputFile.close()
+    # printValues()
+
+def popupDialog(title,text):
+    # Create a Toplevel window as the dialog
+    dialog = tk.Toplevel(root)
+    dialog.title(title)
+
+    dialog.resizable(0, 0)
+
+    # Add content to the dialog
+    label = tk.Label(dialog, text=text)
+    label.pack(padx=10, pady=10)
+
+    # Add a button to close the dialog
+    close_button = tk.Button(dialog, text="Close", command=dialog.destroy)
+    close_button.pack(pady=10)
+
+def is_integer(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
 def printValues():
-    print("Spam Dictionary Size: {}, Spam Total Number of words: {} ".format(spamSize, spamNumWords))
+    print("k: {}".format(k))
+    print("Spam Dictionary Size: {}, Spam Total Number of words: {}, Spam File Count: {}".format(spamSize, spamNumWords, spamCount))
     # print("Spam Dictionary: {} ".format(spamDict))
-    print("Ham Dictionary Size: {}, Ham Total Number of words: {} ".format(hamSize, hamNumWords))
+    print("Ham Dictionary Size: {}, Ham Total Number of words: {}, Ham File Count: {} ".format(hamSize, hamNumWords, hamCount))
     # print("Ham Dictionary: {} ".format(hamDict))
 
 #create window
@@ -223,6 +366,15 @@ wordCountFrame.pack(side=tk.TOP, padx=5, pady=5)
 
 openClassifyfolderbutton = tk.Button(classifyFrame, text="Select Classify Folder", command=openClassifyFolder)
 openClassifyfolderbutton.pack(side=tk.TOP, pady=5)
+openedClassifyFolderLabel = tk.Label(
+        classifyFrame, 
+        text="Folder to Classify: None",
+        fg="BLACK", bg=bgcolor1,
+        font=('Arial',10,'bold'),
+        wraplength=400
+        )
+openedClassifyFolderLabel.pack(side=tk.TOP, pady=5)
+
 
 kfilterFrame = tk.Frame(classifyFrame, height=100, bg=bgcolor1)
 kLabel = tk.Label(
@@ -251,13 +403,13 @@ outputLabel.pack(side=tk.TOP, padx=5, pady=5)
 
 outputTableFrame = tk.Frame(classifyFrame,bg=bgcolor1)
 
-outputTable = ttk.Treeview(outputTableFrame, columns=("filename","word", "frequency"), show='headings')
+outputTable = ttk.Treeview(outputTableFrame, columns=("filename","class","probability"), show='headings')
 outputTable.heading("filename", text="Filename")
-outputTable.heading("word", text="Word")
-outputTable.heading("frequency", text="Frequency")
+outputTable.heading("class", text="Class")
+outputTable.heading("probability", text="P(spam)")
 outputTable.column("filename", width=100, anchor="center") 
-outputTable.column("word", width=150, anchor="center") 
-outputTable.column("frequency", width=30, anchor="center") 
+outputTable.column("class", width=50, anchor="center") 
+outputTable.column("probability", width=150, anchor="center") 
 
 outputvsb = ttk.Scrollbar(outputTableFrame, orient="vertical", command=outputTable.yview)
 outputTable.configure(yscrollcommand=outputvsb.set)
